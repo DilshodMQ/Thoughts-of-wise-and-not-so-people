f43fcbf8623685acba64270b27146cdc6de13915
DilshodMQ <dilshodmahamadiyev1991@gmail.com>
2023-02-25 09:33:53 +0500

    fixed

--- DsrProject/Systems/Api/DsrProject.Api/Bootstrapper.cs
+++ DsrProject/Systems/Api/DsrProject.Api/Bootstrapper.cs
@@ -6,36 +6,27 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class Bootstrapper
 {
-    public static IServiceCollection RegisterAppServices(this IServiceCollection services , WebApplicationBuilder builder)
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services, WebApplicationBuilder builder)
     {
         services
             .AddSwaggerSettings(builder)
             .AddApiSpecialSettings(builder)
             .AddMainSettings(builder);
-      
         return services;
     }
     public static  IServiceCollection AddMainSettings(this IServiceCollection services, WebApplicationBuilder  builder)
-    {
-        
-        services.Configure<MainSettings>
-         (builder.Configuration.GetSection("Main"));
+    {  
+        services.Configure<MainSettings>(builder.Configuration.GetSection("Main"));
         return services;
     }
-
     public static IServiceCollection AddApiSpecialSettings(this IServiceCollection services, WebApplicationBuilder builder)
     {
-
-        services.Configure<ApiSpecialSettings>
-         (builder.Configuration.GetSection("ApiSpecial"));
+        services.Configure<ApiSpecialSettings>(builder.Configuration.GetSection("ApiSpecial"));
         return services;
     }
-
     public static IServiceCollection AddSwaggerSettings(this IServiceCollection services, WebApplicationBuilder builder)
     {
-
-        services.Configure<SwaggerSettings>
-         (builder.Configuration.GetSection("Swagger"));
+        services.Configure<SwaggerSettings>(builder.Configuration.GetSection("Swagger"));
         return services;
     }
 }
